bf211136930fe409fc496eeda02574ed0b2ebd2f
Tariq Rasheed <38127956+xs2tariqrasheed@users.noreply.github.com>
2023-02-20 14:55:30 +0500

    Fonts (#5)
    * added font-family
    
    * font weight added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Inter } from "@next/font/google";
 
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
+const inter = Inter({
+  weight: ["400", "500"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <main className={inter.className}>
+      <div
+        className={classNames(
+          bottomBorder
+            ? "fixed border-b border-black z-[100] w-full flex justify-between items-center bg-off-white"
+            : "fixed z-[100] w-full flex justify-between items-center bg-off-white"
+        )}
+      >
+        <Navbar />
+      </div>
       <Component {...pageProps} />
-    </div>
+    </main>
   );
 }
