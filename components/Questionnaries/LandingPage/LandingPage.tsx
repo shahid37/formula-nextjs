097578170c7753c9ca9d164500a784f4ef3f5985
shahid-hameed-visiata <shahid.hameed@visiata.com>
2023-04-11 21:32:53 +0500

    change

--- components/Questionnaries/LandingPage/LandingPage.tsx
+++ components/Questionnaries/LandingPage/LandingPage.tsx
@@ -1,51 +1,11 @@
-// import Button from "../../../components/Button/Button";
-// import Image from "next/image";
-// import React, { FC } from "react";
 
-// import backgroundImg from "../../../public/assets/questionnaire.png";
-// import { useRouter } from "next/router";
-
-// interface LandingPageProps {
-//   text?: string;
-// }
-
-// const LandingPage: FC<LandingPageProps> = ({ text }: LandingPageProps) => {
-//   const router = useRouter();
-//   const onClick = () => {
-//     router.push("/questionnaries");
-//   };
-//   return (
-//     <div className="flex relative">
-//       <Image
-//         className="w-screen xs:object-left xs:h-screen md:h-full object-cover"
-//         src={backgroundImg}
-//         alt="side image"
-//       />
-//       <div className="absolute xs:left-4 xs:right-4 md:left-28 xs:bottom-0 md:bottom-28 xs:top-20 md:top-80 flex flex-col xs:justify-end text-white">
-//         <h1 className="xs:w-auto md:w-2/5 xs:text-[32px] xs:leading-[35px] md:text-[64px] md:leading-[64px]">
-//           {text}
-//         </h1>
-//         <h5 className="text-[16px] leading-5 font-light opacity-80 mt-6 xs:w-60 md:w-1/5">
-//           Answer the following questions as best as you can.
-//         </h5>
-//         <div className="mt-20 xs:w-[100%] md:w-80 pb-4">
-//           <Button onClick={onClick}>CREATE MY FORMULA</Button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// LandingPage.defaultProps = {
-//   text: "Creation of your customized pain formula is almost complete!",
-// };
-// export default LandingPage;
 
 import React from "react";
 
 // Components
 import Button from "@/components/Button";
 import { useRouter } from "next/router";
+import usePersistentState from "@/hooks/usePersistentState";
 
 const Container = ({ children, className }: any) => (
   <div
@@ -58,8 +18,21 @@ const Container = ({ children, className }: any) => (
 );
 
 const LandingPage = () => {
-    const router = useRouter();
+
+   const router = useRouter();
+  const [isCreateQuestion, setIsCreateQuestion] = usePersistentState(
+    "isCreateQuestion",
+    false,
+  );
+  const [currentQuestion, setCurrentQuestion] = usePersistentState(
+    "currentQuestion",
+    0
+  );
+
   const onClick = () => {
+    setIsCreateQuestion(false);
+    localStorage.setItem("questionData",'');
+    setCurrentQuestion(0);
     router.push("/questionnaries");
   };
 
@@ -82,7 +55,7 @@ const LandingPage = () => {
             Answer the following questions as best as you can.
           </p>
           <div className="xs:mt-[40px] lg:mt-16 2xl:mt-20 max-w-[358px] w-[100%]">
-            <Button onClick={() => onClick()}>CREATE MY FORMULA</Button>
+            <Button onClick={onClick}>CREATE MY FORMULA</Button>
           </div>
         </div>
       </Container>
