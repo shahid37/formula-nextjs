215cf53da64233323030cda1251b10d1dec070c4
Tariq Rasheed <xs2tariqrasheed@gmail.com>
2023-04-11 11:51:58 +0500

    fixed the desktop landing page issues

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -4,6 +4,8 @@ import classNames from "classnames";
 
 import Hero from "../../public/assets/hero.png";
 
+import { AppMainLayout } from "../MainLayout/AppMainLayout";
+
 import ShiftIcon from "./images/ShiftIcon";
 import VideoIcon from "./images/VideoIcon";
 import MedicineIcon from "./images/MedicineIcon";
@@ -19,97 +21,95 @@ const Main = () => {
 
   return (
     <div className={classNames(commonClassNames, "w-full")}>
-      <div className="mt-[60px] relative w-full xs:h-[350px] md:h-64">
-        <h1 className="xs:px-6 xs:text-[44px] md:px-28 absolute top-[104px] z-[100] left-0 text-white md:text-[40px] max-w-[725px] leading-[48px]">
-          Customized prescription medicine for your pain
-        </h1>
+      <div className="mt-[60px] relative w-full md:h-64">
+        <AppMainLayout className={"flex items-end h-full pb-12"}>
+          <h1 className="text-white text-[40px] max-w-[524px] leading-[48px]">
+            Customized prescription medicine for your pain
+          </h1>
+        </AppMainLayout>
         <Image
           src={Hero}
-          className="xs:object-cover xs:h-[100%] md:object-cover"
+          className="relative z-[-100] object-cover h-[100%]"
           alt="Hero image"
           fill
         />
       </div>
-      <div
-        className={classNames(
-          commonClassNames,
-          "xs:pt-6 xs:px-6 md:px-28 md:pt-11"
-        )}
-      >
-        <div className="grid xs:grid-cols-1 md:grid-cols-3 gap-6">
-          {[
-            {
-              text: "Complete 5 minute pain assessment questionnaire",
-              icon: <ShiftIcon />,
-            },
-            {
-              text: "Speak with a licensed doctor or provider over video, phone, or chat",
-              icon: <VideoIcon />,
-            },
-            {
-              text: "Your customized pain formula created and shipped directly to you",
-              icon: <MedicineIcon />,
-            },
-          ].map((item) => (
-            <div key={item.text} className="flex items-center">
-              {item.icon}
-              <h6 className="text-gray text-base ml-4">{item.text}</h6>
+      <AppMainLayout className={""}>
+        <>
+          <div className={classNames(commonClassNames, "xs:pt-6 md:pt-10")}>
+            <div className="grid xs:grid-cols-1 md:grid-cols-3 gap-6">
+              {[
+                {
+                  text: "Complete 5 minute pain assessment questionnaire",
+                  icon: <ShiftIcon />,
+                },
+                {
+                  text: "Speak with a licensed doctor or provider over video, phone, or chat",
+                  icon: <VideoIcon />,
+                },
+                {
+                  text: "Your customized pain formula created and shipped directly to you",
+                  icon: <MedicineIcon />,
+                },
+              ].map((item) => (
+                <div key={item.text} className="flex items-center">
+                  <div className="w-12 h-12">{item.icon}</div>
+                  <h6 className="text-gray text-base ml-4">{item.text}</h6>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="pt-10">
-          <h5 className="text-black">Find your perfect PAIN formula</h5>
-          <p className="text-sm font font-normal text-gray">
-            Pick a category to begin creating your formula
-          </p>
-          <div className="flex flex-row justify-between flex-wrap gap-4 mt-4">
-            {[
-              {
-                name: "Muscle Pain",
-                type: "musclePain",
-              },
-              {
-                name: "Joint Pain",
-                type: "jointPain",
-              },
-              {
-                name: "Nerve Pain",
-                type: "nervePain",
-              },
-              {
-                name: "Migraines",
-                type: "migraines",
-              },
-            ].map((item) => (
-              <div
-                className={classNames(
-                  commonClassNames,
-                  "items-center xs:basis-[46%] md:basis-[23%]"
-                )}
-                key={item.name}
-              >
-                <PainCategory
-                  name={item.name}
-                  painType={item.type as PainTypes}
-                />
+            <div className="pt-10">
+              <h5 className="text-black">Find your perfect PAIN formula</h5>
+              <p className="text-sm font font-normal text-gray">
+                Pick a category to begin creating your formula
+              </p>
+              <div className="flex flex-row gap-6 mt-4">
+                {[
+                  {
+                    name: "Muscle Pain",
+                    type: "musclePain",
+                  },
+                  {
+                    name: "Joint Pain",
+                    type: "jointPain",
+                  },
+                  {
+                    name: "Nerve Pain",
+                    type: "nervePain",
+                  },
+                  {
+                    name: "Migraines",
+                    type: "migraines",
+                  },
+                ].map((item) => (
+                  <div
+                    className={classNames("items-center w-[25%] grow")}
+                    key={item.name}
+                  >
+                    <PainCategory
+                      name={item.name}
+                      painType={item.type as PainTypes}
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-        </div>
+            </div>
 
-        {/*  Footer */}
-        <div className="mt-10">
-          <Footer />
-        </div>
-        <div className="mx-auto my-10 xs:w-[100%] md:w-96">
-          <Button
-            onClick={() => router.push("/questionnaries/home")}
-            type="default"
-          >
-            Create my Formula
-          </Button>
-        </div>
-      </div>
+            {/*  Footer */}
+            <div className="mt-10">
+              <Footer />
+            </div>
+            <div className="pb-[58px] mx-auto mt-10 xs:w-[100%] md:w-96">
+              <Button
+                onClick={() => router.push("/questionnaries/home")}
+                type="default"
+              >
+                Create my Formula
+              </Button>
+            </div>
+          </div>
+        </>
+      </AppMainLayout>
     </div>
   );
 };
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import classNames from "classnames";
-import React, { FC, useState } from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import React, { FC } from "react";
 
 import Button from "../Button";
 
@@ -12,62 +11,20 @@ interface NavbarProps {
 }
 
 const Navbar: FC<NavbarProps> = ({ showLink, linkAction }: NavbarProps) => {
-  const [nav, setNav] = useState(false);
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
   const commonClassNames =
-    "flex justify-between items-center bg-off-white py-4 xs:px-2 md:px-[120px]";
+    "flex justify-between items-center bg-off-white py-4 xs:px-4 md:px-[120px]";
   return (
     <div className="w-full">
       <nav className={classNames(commonClassNames)}>
         <Logo />
-        <div className="hidden md:flex">
+        <div className="flex">
           {showLink && (
             <Button onClick={linkAction} type="link">
               Create my formula
             </Button>
           )}
         </div>
-        {/* Hamburger Icon */}
-        <div onClick={handleNav} className="md:hidden">
-          <AiOutlineMenu size={25} />
-        </div>
       </nav>
-
-      {/* Mobile Menu */}
-
-      {/* Overlay */}
-      <div
-        className={
-          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
-        }
-      >
-        {/* Side Drawer Menu */}
-        <div
-          className={classNames(
-            nav
-              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-          )}
-        >
-          <div>
-            <div className={classNames(commonClassNames, "w-full")}>
-              <Logo />
-              <div
-                onClick={handleNav}
-                className="rounded ml-4 shadow-lg px-3 pt-1 cursor-pointer"
-              >
-                <AiOutlineClose />
-              </div>
-            </div>
-          </div>
-          <div className="py-4 flex flex-col">
-            <Button onClick={linkAction} type="link">Create my formula</Button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
