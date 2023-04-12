4ccdcd584213ca4ec8f2ccd6c3f77b682f048431
Hasnaat-Iftikhar <hasnaatfreelancing@gmail.com>
2023-04-12 18:30:00 +0500

    Worked on home issue

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -12,9 +12,10 @@ interface FooterProps {
 const Footer: FC<FooterProps> = (props: FooterProps) => {
   return (
     <div className="xs:p-4 md:p-6 rounded-lg border border-light-gray ">
-      <h4 className="hidden sm:block leading-[29px]">{props.headingText}</h4>
-      <h4 className="block sm:hidden leading-[24px] ">{props.headingText}</h4>
-      <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+      <h4 className="xs:text-[20px] xs:leading-[24px] md:text-[24px] md:leading-[29px]">
+        {props.headingText}
+      </h4>
+      <div className="grid xs:grid-cols-2 md:grid-cols-4 gap-6 xs:mt-[24px] md:mt-[34px]">
         {[
           {
             text: "Provides high levels of active ingredient directly to the site of injury minimizing loss in the blood stream",
@@ -36,8 +37,9 @@ const Footer: FC<FooterProps> = (props: FooterProps) => {
           <div className="flex flex-col" key={item.text}>
             {item.icon}
 
-            <h6 className="hidden sm:block mt-1 text-gray leading-5">{item.text}</h6>
-            <h6 className="block sm:hidden mt-1 text-gray leading-[17px] text-sm">{item.text}</h6>
+            <h6 className="mt-2 text-gray xs:text-[14px] xs:leading-[17px] md:text-[16px] md:leading-[19px]">
+              {item.text}
+            </h6>
           </div>
         ))}
       </div>
